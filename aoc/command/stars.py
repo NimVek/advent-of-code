14ba46de2c72770970e0c93e00cd3d85c739e060
NimVek <NimVek@users.noreply.github.com>
2022-04-18 21:00:51 +0200

    resolve B020

--- aoc/command/stars.py
+++ aoc/command/stars.py
@@ -13,11 +13,11 @@ def cmd_stars(args):
     readme = args.base / "README.md"
     with open(readme) as f:
         content = f.read()
-    for year, stars in stars.items():
-        colour = color.color_scale(stars, (0, 50)) if stars else color.LIGHTGREY
+    for year, count in stars.items():
+        colour = color.color_scale(count, (0, 50)) if count else color.LIGHTGREY
         content = re.sub(
-            fr"\(https:\/\/img\.shields\.io\/badge\/{year}-★_[^)]*\)",
-            f"(https://img.shields.io/badge/{year}-★_{stars}-{colour.html})",
+            rf"\(https:\/\/img\.shields\.io\/badge\/{year}-★_[^)]*\)",
+            f"(https://img.shields.io/badge/{year}-★_{count}-{colour.html})",
             content,
         )
     with open(readme, "w") as f:
