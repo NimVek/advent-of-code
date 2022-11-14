9e2777b0d12266c473e971f210febb687c5d6eac
NimVek <NimVek@users.noreply.github.com>
2022-11-14 21:57:32 +0100

    update command updates Readme, input and answers

--- aoc/command/init.py
+++ aoc/command/init.py
@@ -44,6 +44,25 @@ def cmd_init(args):
             f.write(args.api.answers(args.year, args.day))
 
 
+def cmd_update(args):
+    args.current.mkdir(parents=True, exist_ok=True)
+
+    readme = args.current / "README.md"
+    with open(readme, "w") as f:
+        f.write(args.api.mission(args.year, args.day))
+
+    data = args.current / "input"
+    if not data.is_file():
+        with open(data, "w") as f:
+            f.write(args.api.data(args.year, args.day))
+
+    answers = args.current / "answers"
+    with open(answers, "w") as f:
+        f.write(args.api.answers(args.year, args.day))
+
+
 def setup_parser(parent_parser):
     parser = parent_parser.add_parser("init")
     parser.set_defaults(func=cmd_init)
+    parser = parent_parser.add_parser("update")
+    parser.set_defaults(func=cmd_update)
