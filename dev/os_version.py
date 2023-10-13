33bdb536347e7052184024a0789df86abca1f152
NimVek <NimVek@users.noreply.github.com>
2023-10-13 23:49:08 +0200

    update os detection for windows 11

--- dev/os_version.py
+++ dev/os_version.py
@@ -5,28 +5,31 @@ import platform
 if platform.system() == "Linux":
     import os
 
-    os.system("/usr/bin/lsb_release -ds")  # noqa: S605
+    os.system("/usr/bin/lsb_release -ds")
 
 elif platform.system() == "Windows":
     versions = {
-        10240: 1507,
-        10586: 1511,
-        14393: 1607,
-        15063: 1703,
-        16299: 1709,
-        17134: 1803,
-        17763: 1809,
-        18362: 1903,
-        18363: 1909,
-        19041: 2004,
-        19042: "20H2",
-        19043: "21H1",
-        19044: "21H2",
+        10240: (10, 1507),
+        10586: (10, 1511),
+        14393: (10, 1607),
+        15063: (10, 1703),
+        16299: (10, 1709),
+        17134: (10, 1803),
+        17763: (10, 1809),
+        18362: (10, 1903),
+        18363: (10, 1909),
+        19041: (10, 2004),
+        19042: (10, "20H2"),
+        19043: (10, "21H1"),
+        19044: (10, "21H2"),
+        19045: (10, "22H2"),
+        22000: (11, "21H2"),
+        22621: (11, "22H2"),
     }
     result = platform.platform()
     for i in versions:
         if str(i) in result:
-            result = f"Windows-10 ({versions[i]})"
+            result = f"Windows-{versions[i][0]} ({versions[i][1]})"
     print(result)
 
 else:
