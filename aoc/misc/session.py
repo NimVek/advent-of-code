8563fc4c716f137d4f65536cf4314e68a6ccffac
NimVek <NimVek@users.noreply.github.com>
2023-10-23 21:23:49 +0200

    catch all request results

--- aoc/misc/session.py
+++ aoc/misc/session.py
@@ -45,14 +45,18 @@ class CachedSession(Session):
                     return f.read()
 
         result = super().request(method, url, *args, **kwargs)
-        if result.ok:
-            result = result.text
-            if method == "GET":
-                cache_file = self.cache_file(url)
-                with cache_file.open("w") as f:
-                    f.write(result)
-            return result
-        return None
+        result.raise_for_status()
+        result = result.text
+
+        cache_file = self.cache_file(url)
+        if method != "GET":
+            import time
+
+            cache_file = cache_file.with_suffix(f".{method}.{int(time.time())}")
+        with cache_file.open("w") as f:
+            f.write(result)
+
+        return result
 
     def purge(self, url):
         __log__.info("Purge URL %s", url)
