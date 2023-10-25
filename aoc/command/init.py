fecc07dc11427d34ab1c8b112d437ec46fd3fff2
NimVek <NimVek@users.noreply.github.com>
2023-10-25 21:33:56 +0200

    force update

--- aoc/command/init.py
+++ aoc/command/init.py
@@ -11,6 +11,8 @@ def cmd_init(args):
 
 
 def cmd_update(args):
+    if args.force:
+        args.api.User().Puzzle(args.year, args.day).purge()
     args.api.initialize(args.base, args.year, args.day, update=True)
 
 
@@ -23,3 +25,6 @@ def setup_parser(parent_parser):
 
     parser = parent_parser.add_parser("update")
     parser.set_defaults(func=cmd_update)
+    parser.add_argument(
+        "-f", "--force", action="store_true", help="reinitialize all files"
+    )
