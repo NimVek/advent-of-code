9914d5ad74c8d40e859ae8b6e8456927577fefaf
NimVek <NimVek@users.noreply.github.com>
2023-11-11 22:59:36 +0100

    use global interval definition

--- y2022/d04/solution.py
+++ y2022/d04/solution.py
@@ -1,7 +1,6 @@
 import functools
 
-from dataclasses import dataclass
-
+from aoc.lib.sets import Interval
 from aoc.lib.solution import SolutionBase
 
 import logging
@@ -11,12 +10,6 @@ __all__ = ["Solution"]
 __log__ = logging.getLogger(__name__)
 
 
-@dataclass
-class Interval:
-    lower: int
-    upper: int
-
-
 class Solution(SolutionBase):
     @staticmethod
     def prepare(data):
@@ -24,7 +17,7 @@ class Solution(SolutionBase):
         for line in data:
             result.append(
                 [
-                    Interval(*map(int, assignment.split("-")))
+                    Interval(map(int, assignment.split("-")))
                     for assignment in line.split(",")
                 ]
             )
@@ -36,16 +29,11 @@ class Solution(SolutionBase):
 
     @staticmethod
     def contains(first, second):
-        return (
-            first.lower <= second.lower
-            and first.upper >= second.upper
-            or first.lower >= second.lower
-            and first.upper <= second.upper
-        )
+        return first in second or second in first
 
     @staticmethod
     def overlaps(first, second):
-        return first.upper >= second.lower and first.lower <= second.upper
+        return bool(first & second)
 
     part_01 = functools.partial(generic, evaluation=contains)
     part_02 = functools.partial(generic, evaluation=overlaps)
